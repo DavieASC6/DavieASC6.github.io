345c3d8b073e81757bd31eeb3d946287d5cc5349
dnite2477@gmail.com <DavieASC6>
2019-08-02 15:26:32 -0400

    nj

---
+++ sketch.js
@@ -0,0 +1,43 @@
+// function textGrey() {
+//     let grey = document.querySelector("nav-bg");
+//     grey.style.color = "#CDCDCD";
+// }
+
+// let navbg = document.getElementById("navbg");
+// navbg.addEventListener("click", textGrey)
+
+// function number() {
+//     const introParagraph = document.createElement("p");
+//     introParagraph.innerText = "201-678-7890";
+//     introParagraph.style.backgroundColor = "#FFFFFF";
+// }
+
+// let overlay = document.getElementById("overlay");
+// overlay.addEventListener("click", number);
+
+// const david = {
+//     powerlevel: 9001,
+//     traits: ['good students', 'responsible', 'on time'],
+//     firstName: 'David',
+//     lastName: 'Johny',
+//     sayName: function() {
+//         console.log(`My name is ${this.firstName} ${this.lastName}`);
+//     }
+// }
+
+// david.sayName();
+
+// const malachi = {
+//     likes: ['taco', 'osu', 'memes', 'corsair'],
+//     tacoCount: 56,
+//     age: 17,
+//     yearsFromNow: function(years) {
+//         let newAge = this.age + years;
+//         return `In ${years} years, I will be ${newAge}`;
+//     }
+// }
+
+// console.log(malachi.likes[2])
+// console.log(malachi.yearsFromNow(53))
+// malachi.middleName = 'Noel';
+// console.log(malachi);
